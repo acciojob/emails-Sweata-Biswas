c232119a2f0d3f4e28ee4f3a97af840c251d8585
Sweata-Biswas <biswassweata22@gmail.com>
2022-12-24 15:18:20 +0530

    final

--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -2,9 +2,9 @@ package com.driver;
 
 import org.apache.commons.lang3.tuple.Pair;
 
+import java.lang.reflect.Array;
 import java.time.LocalTime;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.*;
 
 public class Workspace extends Gmail{
 
@@ -27,18 +27,28 @@ public class Workspace extends Gmail{
         // 2. If you want to attend a meeting, you must join it at its start time and leave at end time.
         // Example: If a meeting ends at 10:00 am, you cannot attend another meeting starting at 10:00 am
 
-           int maxMeeting = 0;
-            for(int i = 0; i< calendar.size();i++){
-                for(int j = i+1; j < calendar.size()-1; j++){
-                        Meeting index_i = calendar.get(i);
-                        Meeting index_j = calendar.get(j);
-                        int val = index_i.getStartTime().compareTo(index_j.getStartTime());
-//                        System.out.println(val);
-                    if(val>0){
+        Collections.sort(calendar,(o1, o2) -> {
+            return o1.getStartTime() == o2.getStartTime() ? o1.getStartTime().compareTo(o2.getStartTime()) : o1.getEndTime().compareTo(o2.getEndTime());
+        });
+//        Queue<LocalTime> addTime = new LinkedList<>();
+           int maxMeeting = 1;
+            for(int i = 0; i< calendar.size()-1 ;i++) {
+//                System.out.println("start  : " + calendar.get(i).getStartTime());
+//                System.out.println("End  :" + calendar.get(i).getEndTime());
 
-                    }
+//                for (int j = i+1; j < calendar.size() - 1; j++) {
+                    Meeting index_i = calendar.get(i);
+                    Meeting index_next = calendar.get(i+1);
+                    int startval = index_next.getStartTime().compareTo(index_i.getEndTime());
+//                int endval = index_i.getEndTime().compareTo(index_next.getEndTime());
+//                    System.out.println("Startval  " + startval);
+//                System.out.println("endval  "+ endval);
+                    if (startval < 0)
+                        maxMeeting++;
                 }
-            }
+//            }
             return maxMeeting;
+
+
     }
 }
